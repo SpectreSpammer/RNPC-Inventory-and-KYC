7e5eae2c884aba551c7df42f9dfd208edd9a10b1
SpectreSpammer <rnavarra.business@gmail.com>
2025-01-20 01:14:35 +0800

    fix white label error for edit

--- src/main/java/com/rnpc/inventory/controller/ComputerPartsController.java
+++ src/main/java/com/rnpc/inventory/controller/ComputerPartsController.java
@@ -1,18 +1,8 @@
-package com.rnpc.inventory.controller.ComputerPartsController;
-
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-import java.util.Date;
-import java.util.List;
+package com.rnpc.inventory.controller;
 
 import com.rnpc.inventory.dto.ComputerDto;
-import com.rnpc.inventory.repository.ComputerPartsRepository;
 import com.rnpc.inventory.service.ComputerPartsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.rnpc.inventory.entity.Computers;
 
